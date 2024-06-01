51d1c3a50afc5b01c03279362ddc10be6394ea6f
gansu18 <gansu2209@gmail.com>
2024-05-31 22:45:47 -0400

    change text

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -5,13 +5,16 @@ using InverterMon.Server.Persistance;
 using InverterMon.Server.Persistance.Settings;
 using System.Globalization;
 using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-//avoid parsing issues with non-english cultures
+// Avoid parsing issues with non-English cultures
 var cultureInfo = new CultureInfo("en-US");
 CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
 CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
 
-var builder = WebApplication.CreateBuilder();
+var builder = WebApplication.CreateBuilder(args);
 _ = int.TryParse(builder.Configuration["LaunchSettings:WebPort"] ?? "80", out var port);
 builder.WebHost.ConfigureKestrel(o => o.Listen(IPAddress.Any, port));
 
@@ -30,27 +33,33 @@ builder.Services.AddSingleton<UserSettings>();
 builder.Services.AddSingleton<CommandQueue>();
 builder.Services.AddSingleton<JkBms>();
 builder.Services.AddSingleton<Database>();
+
 if (!builder.Environment.IsDevelopment())
 {
     builder.Services.AddHostedService<CommandExecutor>();
     builder.Services.AddHostedService<StatusRetriever>();
 }
+
 builder.Services.AddFastEndpoints();
 
 var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
 }
+
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
 
 // Enable CORS
 app.UseCors("AllowAll");
 
+app.UseRouting();
+app.UseAuthorization();
+
 app.UseFastEndpoints(c => c.Endpoints.RoutePrefix = "api");
+
 app.MapFallbackToFile("index.html");
 
 app.Run();
